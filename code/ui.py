d03c1656b2ba2e309a6c620e9547a0eccd4a981f
La <90055383+paulagrata@users.noreply.github.com>
2024-03-08 16:56:52 -0600

    added mood UI + decreases gradually

--- code/ui.py
+++ code/ui.py
@@ -11,13 +11,14 @@ class UI:
         self.font = pygame.font.Font(UI_FONT,UI_FONT_SIZE)
 
         # bar setup
-        self.health_bar_rect = pygame.Rect(10,10,HEALTH_BAR_WIDTH,BAR_HEIGHT)    #l,t,w,h
-        self.energy_bar_rect = pygame.Rect(10,34,ENERGY_BAR_WIDTH,BAR_HEIGHT)
+        self.health_bar_rect = pygame.Rect(790 - BAR_WIDTH ,570,BAR_WIDTH,BAR_HEIGHT)    #l,t,w,h
+        self.energy_bar_rect = pygame.Rect(790 - BAR_WIDTH ,546,BAR_WIDTH,BAR_HEIGHT)
 
 
     def show_money(self, player):
+        self.font = pygame.font.Font(UI_FONT,30)
         text_surf = self.font.render(f'${player.money}', False, 'Black')
-        text_rect = text_surf.get_rect(midbottom = (SCREEN_WIDTH // 2, SCREEN_HEIGHT - 20))
+        text_rect = text_surf.get_rect(topright=(SCREEN_WIDTH - 10, 10))
 
         pygame.draw.rect(self.display_surface, 'White', text_rect.inflate(10,10),0,6) # 0,6 rounds it.
 
@@ -38,13 +39,18 @@ class UI:
         pygame.draw.rect(self.display_surface,color,current_rect)
         pygame.draw.rect(self.display_surface, UI_BORDER_COLOR,bg_rect,3)
         
-
     def display(self, player):
         #pygame.draw.rect(self.display_surface,'black',self.health_bar_rect)
 
+        # moods
+        moods = ['health', 'energy']        
+        for mood in moods:
+            if getattr(player, mood) <= 30:
+                MOOD_COLORS[mood] = 'Red'
+
         # mood ui
-        self.show_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOR)
-        self.show_bar(player.energy, player.stats['energy'], self.energy_bar_rect, ENERGY_COLOR)
+        self.show_bar(player.health, player.stats['health'], self.health_bar_rect, MOOD_COLORS['health'])
+        self.show_bar(player.energy, player.stats['energy'], self.energy_bar_rect, MOOD_COLORS['energy'])
 
         # money ui
         self.show_money(player)
