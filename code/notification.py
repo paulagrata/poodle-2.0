549a2cd20f3e8c450b98cdb364ecb6f8e70a1208
La <90055383+paulagrata@users.noreply.github.com>
2024-03-20 17:25:59 -0500

    removed calling setup twice

--- code/notification.py
+++ code/notification.py
@@ -3,6 +3,7 @@ from settings import *
 
 class Notification:
     def __init__(self, toggle_notification, text):
+        
         # general setup
         self.display_surface = pygame.display.get_surface()
         self.toggle_notification = toggle_notification
@@ -11,27 +12,21 @@ class Notification:
         self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
         self.color = "BLACK"
         self.text = text
-        print(text)
         self.subtext = "press X to close"
 
-        # menu
-        self.active = True
-        
         # setup 
+        self.active = True
         self.setup()
 
     def setup(self):
-
+        # font
         self.text_surf = self.font.render(self.text, False, 'Black')
         self.subtext_surf = self.font.render(self.subtext, False, 'Black')
-
-        # Create rects for the text and subtext
+        # position 
         self.text_rect = self.text_surf.get_rect(bottomright=(self.display_surface.get_width() - 15, self.display_surface.get_height() - 130))
         self.subtext_rect = self.subtext_surf.get_rect(bottomright=(self.display_surface.get_width() - 15, self.display_surface.get_height() - 110))
-
-        # Draw a rectangle around both texts
+        # combines in one box
         self.text_and_subtext_rect = self.text_rect.union(self.subtext_rect)
-        pygame.draw.rect(self.display_surface, 'White', self.text_and_subtext_rect.inflate(10, 10),0,6)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -39,11 +34,10 @@ class Notification:
             self.toggle_notification(text='')
 
     def display(self):
+        pygame.draw.rect(self.display_surface, 'White', self.text_and_subtext_rect.inflate(10, 10), 0, 6)
         self.display_surface.blit(self.text_surf, self.text_rect)
         self.display_surface.blit(self.subtext_surf, self.subtext_rect)
             
-        
     def update(self):
-        self.setup()
         self.input()
         self.display()
